6ad4622f34ce07f76bb83875495185b4ce26c2fb
mamarok <mamarok@gmail.com>
2018-11-27 23:32:06 +0100

    Initial commit

---
+++ api/src/main/java/pl/mczarnik/ppmtoolapi/PpmToolApiApplication.java
@@ -0,0 +1,12 @@
+package pl.mczarnik.ppmtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolApiApplication.class, args);
+    }
+}
